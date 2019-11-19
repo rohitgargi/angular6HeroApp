fde327bc3561583da04c635c8d840e59226c737e
Rohit Sharma <rohitgargi0293@gmail.com>
2019-11-19 21:39:55 +0530

    full code

---
+++ src/app/profile-editor/profile-editor.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, Validators, FormArray } from '@angular/forms';
+
+@Component({
+  selector: 'app-profile-editor',
+  templateUrl: './profile-editor.component.html',
+  styleUrls: ['./profile-editor.component.css']
+})
+export class ProfileEditorComponent {
+
+profileForm = this.fb.group({
+  firstName: ['',Validators.required],
+  lastName: [''],
+  address: this.fb.group({
+    street: [''],
+    city: [''],
+    state: [''],
+    zip: ['']
+  }),
+  aliases: this.fb.array([
+    this.fb.control('')
+  ])
+});
+
+get aliases() {
+  return this.profileForm.get('aliases') as FormArray;
+}
+
+addAlias(){
+  this.aliases.push(this.fb.control(''));
+}
+
+  constructor(private fb:FormBuilder){
+
+  }
+  onSubmit() : void{
+    console.warn(this.profileForm.value);
+  }
+  updateProfile() {
+    this.profileForm.patchValue({
+      firstName: 'Nancy',
+      address: {
+        street: '123 Drew Street'
+      }
+    });
+  }
+}
